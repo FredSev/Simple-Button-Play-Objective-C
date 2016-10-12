154d172ea9999f5d91dfb4e861b957bd8fbeb3c2
iOS MAC <iosmac@iOSs-MacBook-Pro.local>
2016-10-12 09:13:37 -0400

    Initial Commit

---
+++ BtnSpar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BtnSpar
+//
+//  Created by iOS MAC on 10/12/16.
+//  Copyright © 2016 iOS MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
